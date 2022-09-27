ec88d783dd001533c9ab999a0e7071277d4ee151
Igal Klebanov <igalklebanov@gmail.com>
2022-09-27 18:18:53 +0300

    wrong return types in SinglestoreDataApiDialect.

--- src/dialect/data-api/data-api-dialect.ts
+++ src/dialect/data-api/data-api-dialect.ts
@@ -1,4 +1,13 @@
-import {MysqlIntrospector, MysqlQueryCompiler, type Dialect, type Kysely} from 'kysely'
+import {
+  MysqlIntrospector,
+  MysqlQueryCompiler,
+  type DatabaseIntrospector,
+  type Dialect,
+  type DialectAdapter,
+  type Driver,
+  type Kysely,
+  type QueryCompiler,
+} from 'kysely'
 
 import {SinglestoreDataApiAdapter} from './data-api-adapter.js'
 import {SinglestoreDataApiDriver} from './data-api-driver.js'
@@ -11,19 +20,19 @@ export class SinglestoreDataApiDialect implements Dialect {
     this.#config = config
   }
 
-  createAdapter(): SinglestoreDataApiAdapter {
+  createAdapter(): DialectAdapter {
     return new SinglestoreDataApiAdapter()
   }
 
-  createDriver(): SinglestoreDataApiDriver {
+  createDriver(): Driver {
     return new SinglestoreDataApiDriver(this.#config)
   }
 
-  createIntrospector(db: Kysely<any>): MysqlIntrospector {
+  createIntrospector(db: Kysely<any>): DatabaseIntrospector {
     return new MysqlIntrospector(db)
   }
 
-  createQueryCompiler(): MysqlQueryCompiler {
+  createQueryCompiler(): QueryCompiler {
     return new MysqlQueryCompiler()
   }
 }
