be8f763131c6e328ca7ff2000bc0483a06dc77b4
Igal Klebanov <igalklebanov@gmail.com>
2022-10-03 01:50:13 +0300

    ...

---
+++ tests/init.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE test;
+
+USE test;
+
+CREATE ROWSTORE TABLE person (
+    id INTEGER UNSIGNED PRIMARY KEY AUTO INCREMENT,
+    first_name VARCHAR(255),
+    middle_name VARCHAR(255),
+    last_name VARCHAR(255),
+    age DOUBLE NOT NULL,
+    gender VARCHAR(6) NOT NULL
+);
+
+CREATE ROWSTORE TABLE pet (
+    id INTEGER PRIMARY KEY AUTO INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    owner_id INTEGER UNSIGNED NOT NULL,
+    species VARCHAR(7) NOT NULL,
+    KEY pet_owner_id_index (owner_id) USING HASH
+);
+
+CREATE ROWSTORE TABLE toy (
+    id INTEGER PRIMARY KEY AUTO INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(13,4) UNSIGNED NOT NULL,
+    pet_id INTEGER UNSIGNED NOT NULL
+);
+
+INSERT INTO person (id, first_name, last_name, gender, age)
+VALUES 
+    (1, 'Jennifer', 'Aniston', 'female', 25),
+    (2, 'Arnold', 'Schwarzenegger', 'male', 46),
+    (3, 'Syvester', 'Stallone', 'male', 75);
+
+INSERT INTO pet (id, name, owner_id, species)
+VALUES 
+    (1, 'Catto', 1, 'cat'),
+    (2, 'Doggo', 2, 'dog'),
+    (3, 'Hammo', 3, 'hamster');
