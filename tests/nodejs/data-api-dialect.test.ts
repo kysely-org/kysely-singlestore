d4288ca4e54fd4e15504f3235dc290ba585e3eaa
Igal Klebanov <igalklebanov@gmail.com>
2022-10-04 01:06:15 +0300

    ...

--- tests/nodejs/data-api-dialect.test.ts
+++ tests/nodejs/data-api-dialect.test.ts
@@ -1,16 +1,18 @@
 import {expect} from 'chai'
-import {Kysely} from 'kysely'
+import {Kysely, sql} from 'kysely'
 import nodeFetch from 'node-fetch'
 import {fetch as undiciFetch} from 'undici'
 
-import {SingleStoreDataApiDialect} from '../..'
+import {SingleStoreDataApiDialect} from '../../src/dialect/data-api/data-api-dialect'
 
-type Database = {
-  person: {
-    id: number
-    first_name: string | null
-    last_name: string | null
-  }
+interface Database {
+  person: Person
+}
+
+interface Person {
+  id: number
+  first_name: string | null
+  last_name: string | null
 }
 
 describe('SingleStoreDataApiDialect', () => {
@@ -34,6 +36,20 @@ describe('SingleStoreDataApiDialect', () => {
 
       expect(people).to.be.an('array').with.length.greaterThan(0)
     })
+
+    it('should execute raw select queries.', async () => {
+      const {rows: people} = await sql<Person>`select * from ${sql.table('person')}`.execute(db)
+
+      expect(people).to.be.an('array').with.length.greaterThan(0)
+    })
+
+    it('should execute raw with...select queries.', async () => {
+      const {rows: jennifers} = await sql<Person>`with jennifers as (select * from ${sql.table(
+        'person',
+      )} where ${sql.ref('first_name')} = ${sql.literal('Jennifer')}) select * from jennifers`.execute(db)
+
+      expect(jennifers).to.be.an('array').with.length.greaterThan(0)
+    })
   })
 
   describe.skip('insert queries', () => {
@@ -52,61 +68,6 @@ describe('SingleStoreDataApiDialect', () => {
     // TODO: ...
   })
 })
-
-// it('should execute raw select queries.', async () => {
-//   const query = CompiledQuery.raw('select * from `person`')
-//   const response: SingleStoreDataApiQueryTuplesResponseBody<{
-//     id: number
-//     first_name: string | null
-//     last_name: string | null
-//   }> = {
-//     results: [
-//       {
-//         columns: [
-//           {dataType: SingleStoreDataType.BigInt, name: 'id', nullable: false},
-//           {dataType: SingleStoreDataType.Varchar, name: 'first_name', nullable: true},
-//           {dataType: SingleStoreDataType.Varchar, name: 'last_name', nullable: true},
-//         ],
-//         rows: [
-//           {id: 1, first_name: 'Jennifer', last_name: 'Aniston'},
-//           {id: 3, first_name: 'Michael', last_name: 'Jordan'},
-//         ],
-//       },
-//     ],
-//   }
-
-//   interceptQuerySuccess(interceptor, response)
-
-//   const result = await connection.executeQuery(query)
-
-//   expect(result).to.be.an('object')
-//   expect(result.rows).to.deep.equal(response.results[0].rows)
-// })
-
-// it('should execute raw with...select queries.', async () => {
-//   const query = CompiledQuery.raw('with zzz as (select 1) select * from zzz')
-//   const response: SingleStoreDataApiQueryTuplesResponseBody<{1: number}> = {
-//     results: [
-//       {
-//         columns: [
-//           {
-//             dataType: SingleStoreDataType.Int,
-//             name: '1',
-//             nullable: false,
-//           },
-//         ],
-//         rows: [{1: 1}],
-//       },
-//     ],
-//   }
-
-//   interceptQuerySuccess(interceptor, response)
-
-//   const result = await connection.executeQuery(query)
-
-//   expect(result).to.be.an('object')
-//   expect(result.rows).to.deep.equal(response.results[0].rows)
-// })
 
 export function getFetch() {
   const {version} = process
